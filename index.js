624d4ec14a470422345da6ff7b17271a355dc956
karishma-c <karishmadgct@gmail.com>
2021-03-07 11:40:51 +0530

    two more products are added with js functionality

--- index.js
+++ index.js
@@ -1,83 +1,68 @@
-var products = {
-    productimage: "./asset/images/dress2.jpg",
-    productname: "Party Wear Gown - Red",
-    productprice: "800"
-}
+var products = [
+    {
+        productimage: "./asset/images/dress1.jpg",
+        productname: "Party Wear Gown - Red",
+        productprice: "800"
+    },
+    {
+        productimage: "./asset/images/dress2.jpg",
+        productname: "Lehenga choli - Pink",
+        productprice: "1200"
+    },
+    {
+        productimage: "./asset/images/dress3.jpg",
+        productname: "Netted Saree - Grey",
+        productprice: "1000"
+    }
+];
+console.log(products);
 
 const container = document.getElementsByClassName("container")[0];
+const modalContent = document.getElementsByClassName("modal-content")[0];
 const navBar = document.getElementsByClassName("navBar")[0];
-const buyBtn = document.getElementsByClassName("btn")[0];
 const num = document.querySelector(".circle");
+const clickBtn = document.querySelectorAll(".btn");
+var a = document.getElementsByClassName("product-card")
 let initialValue = 0;
 
-buyBtn.addEventListener("click", () => {
+    clickBtn.forEach(buyBtn => {
+        buyBtn.addEventListener('click', () => {
+          let value = num.textContent;
+            if((value == 0) || (value <= 4)) {
+                initialValue++;
+                num.textContent = initialValue;
+                console.log(initialValue);
+            }
+            else if(value == 5) {
+                num.textContent = value + '+';
+                initialValue++;
+                console.log(initialValue);
+            }
+            else {
+                initialValue++;
+                console.log(initialValue);
+            } 
+            
+            products.forEach(product => {
+                const Card = `<div class="product-card">
+                    <div class="product-image">
+                        <img src="${product.productimage}" alt="img">
+                    </div>
+                    <div class="product-details">
+                        <h2 class="product-name">${product.productname}</h2>
+                        <h4 class="product-price"> ${product.productprice}  * ${initialValue}</h4>
+                       <h1 class="total-price">Total Price : ${product.productprice  * initialValue}</h1>
+                    </div>
+                </div>`;     
+                const position = "beforeend";
+                modalContent.insertAdjacentHTML(position, Card);
+            })
+        })
+        
+        var modal = document.getElementById("myModal");
+        setTimeout(function() { 
+            modal.remove();
+        }, 3000);
+    })    
+      
 
-    let value = num.textContent;
-    if((value == 0) || (value <= 4)) {
-        initialValue++;
-        num.textContent = initialValue;
-        console.log(initialValue);
-    }
-    else if(value == 5) {
-        num.textContent = value + '+';
-        initialValue++;
-        console.log(initialValue);
-    }
-    else {
-        initialValue++;
-        console.log(initialValue);
-    }
-
-    const modal = document.createElement("div");
-    var attributes = ["class", "id"];
-    var values = ["modal", "myModal"];
-    for (var i = 0; i < attributes.length; i++) {
-    modal.setAttribute(attributes[i], values[i])
-    }
-
-    const modalContent = document.createElement("div");
-    modalContent.setAttribute("class", "modal-content");
-
-    const productCard = document.createElement("div");
-    productCard.setAttribute("class", "product-card");
-
-    const productImage = document.createElement("div");
-    productImage.setAttribute("class", "product-image");
-
-    const Image = document.createElement("img");
-    Image.src = `${products.productimage}`;
-    Image.setAttribute("class", "product-img");
-
-    const productDetails = document.createElement("div");
-    productDetails.setAttribute("class", "product-details");
-
-    const productName = document.createElement("h2");
-    productName.setAttribute("class", "product-name");
-    productName.textContent = 'Price : ' + `${products.productname}`;
-    
-    const productPrice = document.createElement("h4");
-    productPrice.setAttribute("class", "product-price");
-    // productPrice.textContent = `${products.productprice} * ${initialValue}`;
-    productPrice.textContent = products.productprice + ' * ' + initialValue;
-
-    let tprice = products.productprice  *  initialValue;
-    console.log(tprice);
-    const totalPrice = document.createElement("h1");
-    totalPrice.setAttribute("class", "total-price");
-    totalPrice.textContent = 'Total Price : ' +  `${tprice}`;
-
-    productCard.appendChild(productImage);
-    productCard.appendChild(productDetails);
-    productImage.appendChild(Image);
-    productDetails.appendChild(productName);
-    productDetails.appendChild(productPrice);
-    productDetails.appendChild(totalPrice);
-    modalContent.appendChild(productCard);
-    modal.appendChild(modalContent);
-    navBar.appendChild(modal);
-
-    setTimeout(function() { 
-        navBar.removeChild(modal);
-    }, 3000);
-    
-});
